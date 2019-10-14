824a74c64fce84d1f4f2acc1903cc0a1d720ef97
Tomi Poveda <tpovedatd@gmail.com>
2019-10-14 21:54:39 +0200

    Added simple version test

--- tests/test_changelog.py
+++ tests/test_changelog.py
@@ -1,5 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module that contains tests for artellapipe-tools-changelog
+"""
+
 import pytest
 
+from artellapipe.tools.changelog import __version__
 
-def test_hello():
-    return True
+
+def test_version():
+    assert __version__.__version__
